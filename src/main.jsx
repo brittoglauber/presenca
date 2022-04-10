1c8474b6e7158e46db845115078653441874fbec
Glauber Lucas <brittoglauber@gmail.com>
2022-04-10 10:55:22 -0300

    Subindo repositório

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Home } from './pages/Home'
+import './styles/global.css'
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
